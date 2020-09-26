fe69efda1393697d6bf9aba538b796037a59dc0d
4ipp <4ippers@example.com>
2020-09-26 19:01:28 +0500

    Update keyboard

--- telegos/models/__init__.py
+++ telegos/models/__init__.py
@@ -2,5 +2,5 @@ from telegos.models.base import BaseModel
 from telegos.models.message import MessageModel
 from telegos.models.user import UserModel
 from telegos.models.parse_mode import ParseMode
-from telegos.models.keyboard import KeyboardButton, InlineKeyButton
+from telegos.models.keyboard import KeyboardButton, InlineKeyButton, ButtonTypes, Button
 from telegos.models.callback_query import CallBackQueryModel
