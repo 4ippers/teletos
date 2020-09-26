a58e3db9c9bc1bec25328cd52929aac461d919f0
4ipp <4ippers@example.com>
2020-09-26 18:54:11 +0500

    Update keyboard

--- telegos/models/keyboard.py
+++ telegos/models/keyboard.py
@@ -1,24 +1,49 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import json
 
 
-class KeyboardButton:
-    JOIN_SYMBOL = ";"
+class ButtonTypes:
+    INLINE = "inline_button"
+    KEYBOARD = "keyboard_button"
+
+
+class Button:
+    _JOIN_SYMBOL = ""
+
+    def __init__(self):
+        self.type = None
+
+    def to_dict(self, pass_null=True, pass_internal_attr=True):
+        result = {}
+        for var in self.__dict__:
+            if pass_internal_attr and var.startswith("_"):
+                continue
+
+            if not pass_null:
+                result[var] = self.__dict__[var]
+                continue
+
+            if self.__dict__[var]:
+                result[var] = self.__dict__[var]
+        return result
+
+
+class KeyboardButton(Button):
+    _JOIN_SYMBOL = ";"
 
     def __init__(self, text: str):
-        self.text = text
-
-    def to_dict(self):
-        keys = ""
-        if self.text:
-            keys = f"{self.text}{self.JOIN_SYMBOL}"
-        return keys
+        super().__init__()
+        self.type = ButtonTypes.KEYBOARD
+        self.text = f"{self.text}{self._JOIN_SYMBOL}"
 
 
-class InlineKeyButton:
-    JOIN_SYMBOL = "|"
+class InlineKeyButton(Button):
+    _JOIN_SYMBOL = "|"
 
     def __init__(self, module_name: str, text: str, callback_data: str, url=None):
+        super().__init__()
+        self.type = ButtonTypes.INLINE
         self.text = text
-        self.callback_data = f"{module_name}{self.JOIN_SYMBOL}{callback_data}"
+        self.callback_data = f"{module_name}{self._JOIN_SYMBOL}{callback_data}"
         self.url = url
